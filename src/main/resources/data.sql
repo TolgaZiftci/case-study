82f4daace22307decd6f10d38f96a00de6a9f28a
t00837228 <tolga.ziftci@huawei.com>
2023-05-10 10:19:29 +0300

    initial h2 database setup

---
+++ src/main/resources/data.sql
@@ -0,0 +1,8 @@
+insert into MOVIES values
+(
+ 1, 'Avatar', 'James Cameron'
+);
+insert into MOVIES values
+ (
+  2, 'I Am Legend', 'Francis Lawrence'
+ );
---
+++ src/main/resources/schema.sql
@@ -0,0 +1,6 @@
+create table movies(
+    id integer not null,
+    title varchar(255) not null,
+    director varchar(255) not null,
+    primary key(id)
+);
